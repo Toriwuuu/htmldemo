bdea9ed112e31a834c26e4db9c5b248516e9f40e
Toriwuuu <162669528+Toriwuuu@users.noreply.github.com>
2024-04-25 13:13:14 +0800

    Add files via upload

---
+++ practice2/web2-wed/js/script.js
@@ -0,0 +1,26 @@
+$(function () { /* document.ready */
+
+    // 滑動到指定位子
+    $('.menu a').click(function () {
+        var btn = $(this).attr('href'); /* this=按的那個a */
+        var pos = $(btn).offset();
+        $('html,body').animate({ scrollTop: pos.top }, 1500); /* scrolltop到pos的top，1500毫秒*/
+    });
+
+    // 至頂按鈕
+
+    $('#gotop').click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 1500);
+    });
+
+    // 淡出淡入
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+
+            $('#gotop').stop().fadeTo('', 1);
+
+        } else { $('#gotop').stop().fadeOut(); }
+
+    });
+});
